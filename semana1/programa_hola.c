a8e6b8f5c2c080152481f40d9f4c078bbc924bad
Florian Landa Guadalupe <guadalupe.florian@outlook.com>
2018-08-06 14:56:48 -0500

    Primer ejercicio de clase realizado programa_hola.c

---
+++ semana1/programa_hola.c
@@ -0,0 +1,4 @@
+/* 
+Esto es un comentario, me sirve para escribir información relevante del programa que estoy haciendo. Por ejemplo:
+                      "Programa creado para el 06/08/2018"
+/*
