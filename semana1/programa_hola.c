133c241313700c3bab137eaa0fec9c58db39ccc5
Florian Landa Guadalupe <guadalupe.florian@outlook.com>
2018-08-08 09:30:36 -0500

    Cree mi primer programa

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,4 +1,15 @@
-/* 
-Esto es un comentario, me sirve para escribir información relevante del programa que estoy haciendo. Por ejemplo:
-                      "Programa creado para el 06/08/2018"
-/*
+/* Creado el 6 de Agosto del 2018 por Guadalupe Florian*/
+
+#include<stdio.h>
+
+int main ()
+{
+
+   printf("Hola Lupita\n");
+   printf("Este es el primer programa el curso\n");
+
+}
+
+/*Termine de escribir el programa*/
+ 
+
