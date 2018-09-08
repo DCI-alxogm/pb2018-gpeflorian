ecf84cb242335969b1d049334c4f20c208001425
Guadalupe Sinaí Florián Landa <42146509+gpeflorian@users.noreply.github.com>
2018-09-07 22:10:20 -0500

    Update ejercicio_primo.c

--- semana5/ejercicio_primo.c
+++ semana5/ejercicio_primo.c
@@ -33,46 +33,46 @@ int main(){
 				/*Imprime a la pantalla un aviso para el usuario que el intervalo puede incluir solo un máximo de 100 digitos*/
 				printf("Favor de colocar un intervalo menor a 100"); 
 					
-			printf("Escribe el intervalo de dos numeros dentro del cual deseas saber los numeros primos que existen \n");
+				printf("Escribe el intervalo de dos numeros dentro del cual deseas saber los numeros primos que existen \n");
 				scanf("%i %i",&p1,p2);
 					
 				}
 
-			/*El primer i me indica onde inicia el intervalo, el segundo i me indica donde termina y el i++ me indica que se añade una unidad a la cual se ejecutaran las condiciones dadas para le calculo del resultado*/
-			for(i=p1; i<=p2; i++){ 
+					/*El primer i me indica onde inicia el intervalo, el segundo i me indica donde termina y el i++ me indica que se añade una unidad a la cual se ejecutaran las condiciones dadas para le calculo del resultado*/
+					for(i=p1; i<=p2; i++){ 
 				
-				/*res es igual a la raiz cuadrada del valo de i*/
-				res=sqrt(i); 
+					/*res es igual a la raiz cuadrada del valo de i*/
+					res=sqrt(i); 
 				
-				/*El valor inicial del numero primo es igual a uno*/
-				primo=1; 
+					/*El valor inicial del numero primo es igual a uno*/
+					primo=1; 
 
-				/*Para determinar si un numero es primo, hay que dividir el resultado de la raíz del número. Luego dividirla entre todos los numeros empezando por el 2*/
-				for(j=2; j<=res; j++){ 
+					/*Para determinar si un numero es primo, hay que dividir el resultado de la raíz del número. Luego dividirla entre todos los numeros empezando por el 2*/
+					for(j=2; j<=res; j++){ 
 
-					/*Nos indica la condicion que si el resultado es igual a cero entonces el numero no es primo*/
-					if((i%j)==0){ 
+						/*Nos indica la condicion que si el resultado es igual a cero entonces el numero no es primo*/
+						if((i%j)==0){ 
 					
-					/*Nos ayuda a determinar un valor de primo inicial en cero*/
-					primo=0;
-					}
-					}
+						/*Nos ayuda a determinar un valor de primo inicial en cero*/
+						primo=0;
+						}
+						}
 
-						/*Indica la condicion de que si el residuo de la divicion es un, entonces es numero primo*/
-						if(primo==1){ 
-						/*Imprime a la pantalla los numeros primos dentro del rango dado por el usuario*/
-						printf(" %i",i);
-						/*No ayuda a ver la cantidad de numeros primos impresos*/
-						count=count+1; 
+							/*Indica la condicion de que si el residuo de la divicion es un, entonces es numero primo*/
+							if(primo==1){ 
+							/*Imprime a la pantalla los numeros primos dentro del rango dado por el usuario*/
+							printf(" %i",i);
+							/*No ayuda a ver la cantidad de numeros primos impresos*/
+							count=count+1; 
 		
-							/*Esta condicion if le indica al programa que al llevar al veinteavo termino se saltará a la siguiente línea*/
-							if(count%20==0 ){
-							printf("\n"); 
+								/*Esta condicion if le indica al programa que al llevar al veinteavo termino se saltará a la siguiente línea*/
+								if(count%20==0 ){
+								printf("\n"); 
 									}
 
-					}	
-					}
-
+										}	
+									}
+			
 				/*Pregunta al usuario si desea ejecutar de nuevo el programa o si desea salir de el */
 				printf("\n Introduce 1 si quieres volver a realizar el programa, 2 si quieres salir: \n"); 
 				/*Usuario puede ingresar la opcion que desee*/
