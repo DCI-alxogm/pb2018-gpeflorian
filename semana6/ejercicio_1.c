4456ec492fb601e466b6b387c9a1d7abc2261bd6
Florian Landa Guadalupe <florianlg2018@dci25-175.ugto.mx>
2018-09-10 09:25:16 -0500

    Se terminaron ejemplos

---
+++ semana6/ejercicio_1.c
@@ -0,0 +1,32 @@
+/*Este programa es un ejemplo para la funcion array vista en clase. Creado en Septiempre 10 del 2018.*/
+
+
+/*Me indica las librerias que usare para el programa*/
+#include<stdio.h>
+
+
+/*Funcion maestra del programa*/
+int main (){
+
+		/*Declaro mis variables de tipo flotante y enteras*/
+		int N=10,i;
+		float numeros [N];
+
+			for(i=0;i<N;i++){
+			numeros[i]=0;
+					}
+				
+				/*Asigna el valor a partir de lo que proporciona el usuario linea por linea*/
+				for(i=0;i<N;i++){
+				scanf("%f \n",&numeros[i]);
+					}
+
+					/*Imprimer a la pantalla el valor del elemento i-esimo del arreglo.*/
+					for(i=0;i<N;i++){
+					printf("%f \n",numeros[i]);
+						}
+
+
+	/*Indica si la secuencia de instrucciones sucedio correctamente, de lo contrario enviara signo de error*/				
+	return 0;
+}
