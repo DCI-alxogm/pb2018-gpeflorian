ff847583c53200e379ed3f467779efd0a9e8fdb8
gpeflorian <guadalupe.florian@outlook.com>
2018-09-14 19:57:30 -0500

    Termine programas

--- semana7/ejercicio_est2.c
+++ semana7/ejercicio_est2.c
@@ -8,57 +8,59 @@
 /*Funcion maestra del programa*/
 int main (){
 		
-		FILE *archivo;
-		int N=10,i,sem1=0,sem2=0,sem3=0,sem4=0,sem5=0,sem6=0,sem7=0,sem8=0,sem9=0,sem10=0,M,F
-		float promedios=0;
-		char nombre[255],sexo[255],edad[255],semestre[255],promedio[255]
+		FILE *Informacion_estudiantes;
+		FILE *Resultados_estudiantes;
+		int N=10,i,sem[N],edad[N],sem1=0,sem2=0,sem3=0,sem4=0,sem5=0,sem6=0,sem7=0,sem8=0,sem9=0,sem10=0,sex[N],h=0,f=0;
+		float prom[N],promedios=0;
+
+		Informacion_estudiantes=fopen("Informacion_estudiantes.txt","r");
+
 		
-			archivo=fopen("Informacion_estudiantes.txt","r");
 
 			for(i=0;i<N;i++){
+		
 
-			fgets(semestre,255,(FILE*)archivo);
-			fscanf("%s",semestre);
+			fscanf(Informacion_estudiantes,"%i",&sem[i]);
 
-			if (semestre==1)++sem1;   
-			else if (semestre==2)++sem2;
-			else if(semestre==3)++sem3;
-			else if(semestre==4)++sem4;
-			else if(semestre==5)++sem5;
-			else if(semestre==6)++sem6;
-			else if(semestre==7)++sem7;
-			else if(semestre==8)++sem8;
-			else if(semestre==9)++sem9;
-			else if(semestre==10)++sem10;
+			if (sem[i]==1)++sem1;   
+			else if (sem[i]==2)++sem2;
+			else if(sem[i]==3)++sem3;
+			else if(sem[i]==4)++sem4;
+			else if(sem[i]==5)++sem5;
+			else if(sem[i]==6)++sem6;
+			else if(sem[i]==7)++sem7;
+			else if(sem[i]==8)++sem8;
+			else if(sem[i]==9)++sem9;
+			else if(sem[i]==10)++sem10;
 
-			fgets(promedio,255,(FILE*)archivo);
-			fscanf("%s",promedio);
+			fscanf(Informacion_estudiantes,"%f",&prom[i]);
 
-			promedios=promedios+promedio;
+			promedios=promedios+prom[i];
 
-			fgets(edad,255,(FILE*)archivo)
-			fscanf("%s",edad);
 
-			fgets(sexo,255,(FILE*)archivo);
-			printf("Para sexo masculino presione 0, para sexo femenino persione 1\n");
-			fscanf("%s",sexo);
-			if (sexo==M)++M;
-			else if (sex[i]==F)++F;
+			fscanf(Informacion_estudiantes,"%i",&edad[i]);
 
+			fscanf(Informacion_estudiantes,"%i",&sex[i]);
+			if (sex[i]==0)++h;
+			else if (sex[i]==1)++f;
 
 
 					}
 
-			printf("El numero de estudiates por Semestre son en el 1: %i, en el 2: %i, en el 3: %i, en el 4: %i, en el 5: %i, en el 6: %i, en el 7: %i, en el 8: %i, en el 9: %i y en el 10: %i  \n",sem1,sem2,sem3,sem4,sem5,sem6,sem7,sem8,sem9,sem10);
+			fclose(Informacion_estudiantes);
+
+
+	Resultados_estudiantes=fopen("Resultados_estudiantes.txt","w");
+
+			fprintf(Informacion_estudiantes,"El numero de estudiates por Semestre son en el 1: %i, en el 2: %i, en el 3: %i, en el 4: %i, en el 5: %i, en el 6: %i, en el 7: %i, en el 8: %i, en el 9: %i y en el 10: %i  \n",sem1,sem2,sem3,sem4,sem5,sem6,sem7,sem8,sem9,sem10);
 
 			promedios=promedios/10;
-			printf("El promedio total es %f \n",promedios);
+			fprintf(Informacion_estudiantes,"El promedio total es %f \n",promedios);
 
-			printf("De los estudiantes %i son de sexo femenino y %i son de sexo mascuino\n",f,h);
+			fprintf(Informacion_estudiantes,"De los estudiantes %i son de sexo femenino y %i son de sexo mascuino\n",f,h);
 
-		
+	fclose(Resultados_estudiantes);
 
 
 	return 0;
 	}
-
---
+++ semana7/factorial.c
@@ -0,0 +1,52 @@
+/*Este programa evaluara una variable x en exponencial, logaritmo, seno, coseno y raiz cuadrada en un intervalo y con un espaciado definido por el usuario.*/
+
+/*Se indica la libreria que usamos*/
+#include <stdio.h>
+#include<math.h>
+
+/*Funcion maestra del programa*/
+int main()
+{
+	FILE *factorial;
+	factorial=fopen("factorial.txt","w");
+	/*Me define una varaible de tipo punto flotante res y x*/
+	float xi,xf,espaciado,res;
+	/*Me define una variable de tipo entera n y e que son el intervalo y expaciado*/
+	int i,n;
+
+
+
+		/*Indica al usuario que entre dos valores para calcular x asi como su intervalo*/
+		printf("Ingrese dos valores para el intervalo en el que el valor de x se calculara, asi como el espaciado que habra entre calculo de intervalo  \n");
+		/*Permite al usuario entrar los valores*/
+		scanf(" %f %f %f",&xi,&xf,&espaciado);
+			 
+
+		n=(xf-xi)/espaciado;
+		
+			/*Me indica que las operaciones inician en cero, deben ser menor que n y se le sumara 1*/
+			for(i=0;i<n;i++){
+				/*Realizara las operaciones corresopndientes a el valor x agregado por el usuario*/			
+				res=exp(xi);
+				fprintf(factorial,"El exponencial de %f es %f \n",xi,res);
+				res=log(xi);
+				fprintf(factorial,"El logaritmo de %f es %f \n",xi,res);
+				res=sin(xi)*57.29557795131;
+				fprintf(factorial,"El seno es de %f es %f \n",xi,res);
+				res=cos(xi)*57.29557795131;
+				fprintf(factorial,"El coseno de %f es %f \n",xi,res);
+				res=sqrt(xi);
+				fprintf(factorial,"La raiz cuadrada de %f es %f \n",xi,res);
+				xi=xi+espaciado;	
+
+			
+		                      }
+			fclose(factorial);
+
+		
+			
+	/*Indica si la secuencia de instrucciones sucedio correctamente, de lo contrario enviara signo de error*/
+	return 0;
+
+		
+}
