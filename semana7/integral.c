2207e315b3f9a4034b287ad19292aada3c97c87d
Florian Landa Guadalupe <guadalupe.florian@outlook.com>
2018-09-17 09:56:56 -0500

    Se trabajo en programa integral

--- semana7/integral.c
+++ semana7/integral.c
@@ -4,12 +4,59 @@
 
 /*Me indica las librerias que usare para el programa*/
 #include<stdio.h>
+#include<math.h>
 
 
 /*Funcion maestra del programa*/
 int main (){
 
+	FILE *integral;
+	FILE *resultados;
+	integral=fopen("Integral.txt","r");
+	int P,a,b,N,i;
+	float x,h,I;
 
+		integral=fopen("Integral.txt","r");
+
+		
+		for(i=0;i<N;i++){
+
+			fscanf(integral,"%i",&P);
+
+				if(P<1)
+				printf("El valor de la potencia debe ser igual o mayor a 1 \n");
+				else if(P>30)
+				printf("El valor de P no puede ser mayor a 30");
+					
+		
+			fscanf(integral,"%i",&a);
+
+			fscanf(integral,"%i",&b);
+
+			fscanf(integral,"%i",&N);
+		
+				}
+
+		fclose(integral);
+
+			
+	resultados=fopen("Resultados_integral.txt","w");
+		
+
+		x=exp(P);
+		fprintf(resultados,"%f",x);
+		
+		h=
+
+
+
+
+	fclose(resultados);
+
+		
+			
+
+			
 	/*Indica si la secuencia de instrucciones sucedio correctamente, de lo contrario enviara signo de error*/
 	return 0;
 	}
