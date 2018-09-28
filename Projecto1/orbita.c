602723c7ca8dbf5a9bf9ee40d36a04becaa62ef2
Florian Landa Guadalupe <florianlg2018@dci25-171.ugto.mx>
2018-09-28 13:57:47 -0500

    Se logro elipse

--- Projecto1/orbita.c
+++ Projecto1/orbita.c
@@ -15,7 +15,7 @@ int main (){
 	FILE *resultados;
 	/*Declaro variables tipo enteras y de punto flotante para dar valores de un archivo y ralizar los cálculos para la órbita del planeta*/
 	int M,T;
-	double x0,y0,z0,vx0,vy0,vz0,X,Y,Z,VX,VY,VZ,r,G,m,h,i,count=0;
+	double x0,y0,z0,vx0,vy0,vz0,X,Y,Z,VX,VY,VZ,r,G,m,h,i;
 
 
 	/*Se lee el archivo venus.txt, donde se encuentran valores de x,y,z,X,Y,Z*/
@@ -29,29 +29,25 @@ int main (){
 
 			fscanf(archivo,"%lf %lf %lf",&vx0,&vy0,&vz0);
 
-		
-		
-		/*Se cierra el archivo venus.txt*/
-		fclose(archivo);
+			
 
 			/*Se escribe un archivo llamado resultados_orbita.txt para guardar los resultados de los cálculos realizados*/
 			resultados=fopen("resultados_orbita.txt","w");
 
 				/*Se imprime al documento .txt la clasificación de la información calculada por columna*/
 				fprintf(resultados,"  Día         X          Y          Z         VX         VY         VZ \n");
-				
-				/*Se calcula la constante gravitacional en unidades astronómicas*/
-				G=4.0*pow(3.141592653589,2);
-				
-				/*Se convierte la velocidad de unidades de tiempo días a años para empatar con las demás cifras que están en unidades atronómicas que serán usadas para los cálculos de la órbita*/
-				vx0*=365.252
-				vy0*=365.252
-				vz0*=365.252
+
+			/*Calculo la constante de gravitación en años que es 4*pi cuadrada*/
+			G=4.0*pow(3.141592653589,2);			
+
+
+			/*Convierto las velocidades iniciales a años multiplicando por 365*/
+			vx0*=365.242;
+			vy0*=365.242;
+			vz0*=365.242;
+
 				/*Se inicia un ciclo for en cero, que será menor o igual al tiempo T y tendrá un espaciado (se le sumará) de h en cada cálculo*/
-				for(i=0.0;i<=T+h;i+=h){
-					
-					/*Se calcula el radio que es la raíz de la suma de las posiciones iniciales al cuadrado*/
-					r=sqrt(pow(x0,2.0)+pow(y0,2.0)+pow(z0,2.0));
+				for(i=0.0;i<=T;i+=h){
 						
 					/*Se hacen los cálculos para la posición siguiente usando el espaciado h, que es igual a la posición inicial mas la velocidad inicial (en x, y o z) por h*/
 					X=x0+vx0*h;
@@ -59,6 +55,10 @@ int main (){
 					Y=y0+vy0*h;
 
 					Z=z0+vz0*h;
+
+					/*Se calcula el radio que es la raíz de la suma de las posiciones iniciales al cuadrado*/
+					r=sqrt(pow(x0,2.0)+pow(y0,2.0)+pow(z0,2.0));
+
 
 					/*Se hacen los cálculos de la veolcidad que sigue usando el espaciado h, que es igual al producto de G, M y H dividido entre el radio al cubo, todo esto multiplicado por h y finalmente se resta de la velocidad inicial ya sea en x, y o z.*/
 					VX=vx0-h*((G*M*x0)/pow(r,3.0));
@@ -68,11 +68,18 @@ int main (){
 					VZ=vz0-h*((G*M*z0)/pow(r,3.0));
 					
 					/*Se indica al programa que en cada repetición del ciclo for la x, y o z finales se convertiran en las nuevas x, y o z iniciales, y así sucesivamente*/
-					x0=X
+					x0=X;
 					
-					y0=Y
+					y0=Y;
 						
-					z0=Z
+					z0=Z;
+
+					vx0=VX;
+
+					vy0=VY;
+
+					vz0=VZ;
+
 					
 						/*Se imprime el día al archivo de texto FILE resultados por repeticíón del ciclo*/
 						fprintf(resultados,"%lf ",i*365);
@@ -83,6 +90,10 @@ int main (){
 				}	
 			/*Se cerra el archivo de texto FILE resultados*/
 			fclose(resultados);
+
+
+		/*Se cierra el archivo venus.txt*/
+		fclose(archivo);
 			
 
 
